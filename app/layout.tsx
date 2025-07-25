9b611d0a69587a75046e695958a2c77c869c9b32
Agung Hutri <agung@qtasmin.com>
2025-07-25 23:11:12 +0700

    tes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,19 @@
 "use client";
 import "./globals.css";
+
 import { SessionProvider } from "next-auth/react";
+import { Suspense } from "react";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="id">
+    <html lang="en">
       <body>
-        <SessionProvider>{children}</SessionProvider>
+        <SessionProvider>
+          <Suspense fallback={<div>Loading...</div>}>
+            {children}
+          </Suspense>
+        </SessionProvider>
       </body>
     </html>
   );
 }
-
-
